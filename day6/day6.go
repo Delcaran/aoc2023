2e80658877bf50eb43c3cbd887958c173a0bb759
PAOLUZZIM <matteo.paoluzzi@leonardo.com>
2023-12-11 10:43:27 +0100

    day 6 part 2 test solved

--- day6/day6.go
+++ day6/day6.go
@@ -18,7 +18,7 @@ type race struct {
 	methods []method
 }
 
-func (r *race) estimate() {
+func (r *race) estimate_brute_force() {
 	for release_ms := 1; release_ms < r.time; release_ms++ {
 		m := method{speed: release_ms, release_ms: release_ms, distance: release_ms * (r.time - release_ms)}
 		if m.distance > r.record {
@@ -29,12 +29,12 @@ func (r *race) estimate() {
 
 func (r *race) margin_of_error() int {
 	if len(r.methods) == 0 {
-		r.estimate()
+		r.estimate_brute_force()
 	}
 	return len(r.methods)
 }
 
-func initialize(content string) []race {
+func part1(content string) int {
 	races := make([]race, 0)
 	lines := strings.Split(content, "\n")
 	times := strings.Fields(lines[0])
@@ -50,10 +50,7 @@ func initialize(content string) []race {
 		}
 		races = append(races, race{time: time, record: distance})
 	}
-	return races
-}
 
-func part1(races []race) int {
 	margin := 1
 	for _, r := range races {
 		margin *= r.margin_of_error()
@@ -61,7 +58,22 @@ func part1(races []race) int {
 	return margin
 }
 
+func part2(content string) int {
+	lines := strings.Split(content, "\n")
+	time, err := strconv.Atoi(strings.Join(strings.Fields(strings.Split(lines[0], ":")[1]), ""))
+	if err != nil {
+		log.Fatal(err)
+	}
+	distance, err := strconv.Atoi(strings.Join(strings.Fields(strings.Split(lines[1], ":")[1]), ""))
+	if err != nil {
+		log.Fatal(err)
+	}
+	race := race{time: time, record: distance}
+	margin := race.margin_of_error()
+
+	return margin
+}
+
 func Run(content string) (int, int, error) {
-	races := initialize(content)
-	return part1(races), 0, nil
+	return part1(content), part2(content), nil
 }
