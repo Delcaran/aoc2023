4cf40edcbc8c31f89b76558f6a9ee9c4a7700850
PAOLUZZIM <matteo.paoluzzi@leonardo.com>
2023-12-13 12:10:19 +0100

    day 9 part 1 solved

--- day9/day9.go
+++ day9/day9.go
@@ -1,7 +1,63 @@
 package day9
 
+import (
+	"log"
+	"strconv"
+	"strings"
+)
+
+type sequence struct {
+	values     []int
+	all_zeroes bool
+	diff       *sequence
+}
+
+func (s *sequence) init(data []int, all_zeroes bool) {
+	s.all_zeroes = all_zeroes
+	if s.all_zeroes {
+		s.values = nil // don't waste space for a sequence of zeroes
+		s.diff = nil
+	} else {
+		s.values = append(s.values, data...)
+		var diffs []int
+		all_zeroes := true
+		for x := 1; x < len(s.values); x++ {
+			diff := s.values[x] - s.values[x-1]
+			diffs = append(diffs, diff)
+			all_zeroes = all_zeroes && diff == 0
+		}
+		s.diff = &sequence{}
+		s.diff.init(diffs, all_zeroes)
+	}
+}
+
+func (s *sequence) get_next() int {
+	if s.all_zeroes {
+		return 0
+	} else {
+		return s.values[len(s.values)-1] + s.diff.get_next()
+	}
+}
+
 func Part1(content string) int {
-	return 0
+	sum := 0
+	for _, line := range strings.Split(content, "\n") {
+		if len(line) > 0 {
+			sequence_str := strings.Fields(line)
+			var numlist []int
+			for _, str := range sequence_str {
+				num, err := strconv.Atoi(str)
+				if err != nil {
+					log.Fatal(err)
+				}
+				numlist = append(numlist, num)
+			}
+			var sequence sequence
+			sequence.init(numlist, false)
+			sum += sequence.get_next()
+		}
+	}
+	return sum
 }
 
 func Part2(content string) int {
